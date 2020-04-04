b060dbf0731afa3a67e03c6947731f0c5bd4b2b9
Abdulkader Freij <abdulkader@arch.localdomain>
2020-04-04 12:18:05 +0300

    update

--- server/routes/api.php
+++ server/routes/api.php
@@ -3,16 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
@@ -21,15 +11,19 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('/login', 'UserController@login')->name('login');
 Route::post('/register', 'UserController@register');
 
+Route::get('/currencies', 'CurrencyController@index')->name('currencies.all');
+
 Route::get('/transactions', 'TransactionController@index')->name('transactions.all');
-
 Route::post('/transactions', 'TransactionController@store')->name('transactions.store');
-
 Route::get('/transactions/{transaction}', 'TransactionController@show')->name('transactions.show');
-
 Route::put('/transactions/{transaction}', 'TransactionController@update')->name('transactions.update');
-
 Route::delete('/transactions/{transaction}', 'TransactionController@destroy')->name('transactions.destroy');
+
+Route::get('/categories', 'CategoryController@index')->name('categories.all');
+Route::post('/categories', 'CategoryController@store')->name('categories.store');
+Route::get('/categories/{category}', 'CategoryController@show')->name('categories.show');
+Route::put('/categories/{category}', 'CategoryController@update')->name('categories.update');
+Route::delete('/categories/{category}', 'CategoryController@destroy')->name('categories.destroy');
 
 Route::group(['middleware' => 'auth:api'], function(){
     Route::get('/logout', 'UserController@logout')->name('logout');
